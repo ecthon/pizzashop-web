191e1111b1d599e13b81df19b68749a4bcf04e2d
ecthon <ecthon@gmail.com>
2024-01-12 00:43:26 -0400

    feature: toast notification with sonner

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './global.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 import { HelmetProvider, Helmet} from 'react-helmet-async'
+import { Toaster } from 'sonner'
 
 export function App() {
 
   return (
     <HelmetProvider>
       <Helmet titleTemplate='%s | pizza.shop'/>
+      <Toaster richColors/>
       <RouterProvider router={router} />
     </HelmetProvider>
   )
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,10 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import { resolve } from 'path'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
+import { toast } from 'sonner'
 
 const signInForm = z.object({
     email: z.string().email(),
@@ -16,9 +16,19 @@ export function SignIn(){
     const { register, handleSubmit, formState: { isSubmitting } } = useForm<SignInForm>()
     
     async function handleSignIn(data: SignInForm) {
-        console.log(data)
-        await new Promise((resolve) => setTimeout(resolve, 2000))
+        try {     
+            await new Promise((resolve) => setTimeout(resolve, 2000))
+            toast.success('Enviamos um link de autenticação para o seu e-mail.', {
+                action: {
+                    label: 'Reenviar',
+                    onClick: () => handleSignIn(data),
+                },
+            })
+        } catch {
+            toast.error('Credenciais inválidas.')
+        }
     }
+
     return (
         <>
             <Helmet title='Login'/>
