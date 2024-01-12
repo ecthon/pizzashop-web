b410010f8e4433d62a5da1ea4156e7596a10b9d3
ecthon <ecthon@gmail.com>
2024-01-12 00:15:48 -0400

    feature: get data using React Hook Form

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,9 +1,24 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { resolve } from 'path'
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const signInForm = z.object({
+    email: z.string().email(),
+})
+
+type SignInForm = z.infer<typeof signInForm>
 
 export function SignIn(){
+    const { register, handleSubmit, formState: { isSubmitting } } = useForm<SignInForm>()
+    
+    async function handleSignIn(data: SignInForm) {
+        console.log(data)
+        await new Promise((resolve) => setTimeout(resolve, 2000))
+    }
     return (
         <>
             <Helmet title='Login'/>
@@ -18,12 +33,14 @@ export function SignIn(){
                         </p>
                     </div>
 
-                    <form className='space-y-4'>
+                    <form onSubmit={handleSubmit(handleSignIn)} className='space-y-4'>
                         <div className='space-y-2'>
                             <Label htmlFor='email'>Seu e-mail</Label>
-                            <Input id='email' type='email'/>
+                            <Input id='email' type='email' {...register('email')}/>
                         </div>
-                        <Button type='submit' className='w-full'>Acessar painel</Button>
+                        <Button disabled={isSubmitting} type='submit' className='w-full'>
+                            Acessar painel
+                        </Button>
                     </form>
                 </div>
             </div>
