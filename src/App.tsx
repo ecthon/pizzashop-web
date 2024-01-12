08c79226938f3d8351f5537fa3c093a4684de395
ecthon <ecthon@gmail.com>
2024-01-11 20:51:30 -0400

    React Helmet (SEO)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import { Button } from './components/ui/button'
 import './global.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
+import { HelmetProvider, Helmet} from 'react-helmet-async'
 
 export function App() {
 
-  return <RouterProvider router={router} />
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate='%s | pizza.shop'/>
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  )
 }
 
