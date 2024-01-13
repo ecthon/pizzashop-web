4ad339e774642d1a412ea4f65322e12ae08a0fe7
ecthon <ecthon@gmail.com>
2024-01-13 02:33:38 -0400

    feature: Component: Order Filters

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -3,6 +3,8 @@ import { Input } from "@/components/ui/input";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { ArrowRight, Search, X } from "lucide-react";
 import { Helmet } from "react-helmet-async";
+import { OrderTableRow } from "./order-table-row";
+import { OrderTableFilters } from "./order-table-filters";
 
 export function Orders() {
     return (
@@ -12,10 +14,7 @@ export function Orders() {
                 <h1 className="text-3xl font-bold tracking-tighter">Pedidos</h1>
             </div>
             <div className="space-y-2.5">
-                <form className="flex items-center gap-2">
-                    <span className="text-sm font-semibold">Filtros:</span>
-                    <Input placeholder="Nome do cliente" className="h-8 w-[320px]"/>
-                </form>
+                <OrderTableFilters />
 
                 <div className="border rounded-md">
                     <Table>
@@ -32,40 +31,10 @@ export function Orders() {
                             </TableRow>
                         </TableHeader>
                         <TableBody>
-                            <TableRow>
-                                <TableCell>
-                                    <Button variant="outline" size="xs">
-                                        <Search className="h-3 w-3"/>
-                                        <span className="sr-only">Detalhes do pedido</span>
-                                    </Button>
-                                </TableCell>
-                                <TableCell className="font-mono text-xs font-medium">
-                                    0x09343fdg43f
-                                </TableCell>
-                                <TableCell className="text-muted-foreground">
-                                    há 15 minutos
-                                </TableCell>
-                                <TableCell>
-                                    <div className="flex items-center gap-2">
-                                        <span className="h-2 w-2 rounded-full bg-slate-400" />
-                                        <span className="font-medium text-muted-foreground">Pendente</span>
-                                    </div>
-                                </TableCell>
-                                <TableCell className="font-medium">Ecthon Almeida</TableCell>
-                                <TableCell className="font-medium">R$ 189,90</TableCell>
-                                <TableCell>
-                                    <Button variant="outline" size="xs">
-                                        <ArrowRight className="h-3 w-3 mr-2"/>
-                                        Aprovar
-                                    </Button>
-                                </TableCell>
-                                <TableCell>
-                                    <Button variant="ghost" size="xs">
-                                        <X className="h-3 w-3 mr-2"/>
-                                        Cancelar
-                                    </Button>
-                                </TableCell>
-                            </TableRow>
+                            {Array.from({ length: 10 }).map((_, i) => (
+                                <OrderTableRow key={i}/>
+                            ))}
+                            
                         </TableBody>
                     </Table>
                 </div>
